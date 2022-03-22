4737581574fb0a6ddf11577d05448801080e41fd
tiagokp97 <tiagopesch@gmail.com>
2022-03-22 17:56:57 -0300

    Utilização api criar cadastro e login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,3 +1,25 @@
+import { API } from '../../js/API.js'
 import { LoginTemplate } from '../../js/templates/LoginTemplate.js'
 
 new LoginTemplate()
+
+
+
+//Função pega dados do usuário, salva no localStorage e efetua login 
+/* .addEventListener('submit', async function(event){
+    
+    event.preventDefault()
+    const inputs = event.target
+    for (let i = 0; i < inputs.length; i++) {
+    const { name, value } = inputs[i]
+      if (name) {
+          dataForm[name] = value
+      }
+    }
+
+    await localStorage.setItem('usuario' ,JSON.stringify(dataForm))
+    //const dataUsuario = await User.login("/login", dataForm) 
+    
+  
+  }) */
+
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,3 +1,25 @@
+import { API } from '../../js/API.js'
 import { RegisterTemplate } from '../../js/templates/RegisterTemplate.js'
 
 new RegisterTemplate()
+
+//Função pega dados do usuário, salva no localStorage e efetua registro
+
+/* form.addEventListener('submit', async function(event){
+    
+    event.preventDefault()
+    const inputs = event.target
+    for (let i = 0; i < inputs.length; i++) {
+    const { name, value } = inputs[i]
+      if (name) {
+          dataForm[name] = value
+      }
+    }
+
+    await localStorage.setItem('UsuarioCadastrado', JSON.stringify(dataForm))
+    User.register(dataForm)
+  
+  }) */
+
+
+
