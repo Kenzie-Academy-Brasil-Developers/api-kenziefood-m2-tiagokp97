92e18aa9fc3494bb896686e4c910081defc0c4e8
Wellington José <paivawellington23@gmail.com>
2022-03-22 21:28:20 -0300

    adicionei o modal do perfil com filtro de se estiver logado ou nao

--- src/js/templates/HomeTemplate.js
+++ src/js/templates/HomeTemplate.js
@@ -11,8 +11,14 @@ class HomeTemplate {
     this._button           = document.querySelector('.head-card')
     this._modalCardMobile  = document.querySelector('.container-modal-card')
     this._exitModalCard    = document.querySelector('.exit-modal-card')
+    this._iconPerfil       = document.querySelector('.icon-modal')
+    this._containerLogin   = document.querySelector('.container-modal')
+    this._containerPerfil  = document.querySelector('.container-modal-login')
+
+    this._token            = localStorage.getItem('Kenziefood:token')
 
     this.eventCardMobile()
+    this.verifyLogin()
   }
 
   static getInstance() {
@@ -32,7 +38,30 @@ class HomeTemplate {
     this._exitModalCard.addEventListener('click', (evento) => {
       this._modalCardMobile.style.display = 'none'
     })
+  }
 
+  verifyLogin() {
+    if (this._token) {
+      this._iconPerfil.addEventListener('click', (evento) => {
+        if (this._containerPerfil.style.display === 'none'){
+
+          this._containerPerfil.style.display = 'block'
+        } else {
+
+          this._containerPerfil.style.display = 'none'
+        }
+      })
+    } else {
+      this._iconPerfil.addEventListener('click', (evento) => {
+        if (this._containerLogin.style.display === 'none'){
+
+          this._containerLogin.style.display = 'block'
+        } else {
+
+          this._containerLogin.style.display = 'none'
+        }
+      })
+    }
   }
 
 
