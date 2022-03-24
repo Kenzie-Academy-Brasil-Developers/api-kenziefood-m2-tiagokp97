7ff368934d8cebfb3dabbd1eb8373ca03298723a
Wellington José <paivawellington23@gmail.com>
2022-03-24 20:12:22 -0300

    corrigido o erro de quando clico no produto quando estou logado nao aparece no mobile

--- src/js/templates/HomeTemplate.js
+++ src/js/templates/HomeTemplate.js
@@ -314,6 +314,8 @@ class HomeTemplate {
 
         if (innerWidth > 1100) {
           this.createDesktopLogin()
+        } else {
+          this.createMobileLogin()
         }
 
       }.bind(this))
@@ -332,8 +334,14 @@ class HomeTemplate {
 
         console.log(removeItem)
 
-        this._showcaseDesktop.innerHTML = ''
-        await this.createDesktopLogin()
+        if (innerWidth > 1100) {
+          this._showcaseDesktop.innerHTML = ''
+          await this.createDesktopLogin()
+        } else {
+          this._showcaseModal.innerHTML = ''
+          this.createMobileLogin()
+        }
+
       }.bind(this))
     })
   }
@@ -377,6 +385,32 @@ class HomeTemplate {
         this._showcaseDesktop.appendChild(article)
       })
 
+      let priceTotal = 0
+
+      for (let i = 0; i < getCartModels.length; i++) {
+
+        const { quantity, products } = getCartModels[i]
+
+        const {preco} = products
+
+        priceTotal += preco * quantity
+      }
+
+      let count = 0
+
+      for (let i = 0; i < getCartModels.length; i++) {
+
+        const {quantity, products } = getCartModels[i]
+
+        const {preco} = products
+
+        count += 1 * quantity
+      }
+
+      this._totalPriceDesktop.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(priceTotal)
+
+      this._totalCountDesktop.innerText = count
+
       const buttonRemove = document.querySelectorAll('.button-desktop-card')
       this.captureButtonsRemove(buttonRemove)
     }
@@ -392,6 +426,7 @@ class HomeTemplate {
     }
 
     for (let i = 0; i < getCartModels.length; i++) {
+      console.log(getCartModels[i])
       const { quantity, products } = getCartModels[i]
 
       const { categoria, id, imagem, nome, preco } = products
@@ -418,6 +453,25 @@ class HomeTemplate {
 
       this._showcaseModal.appendChild(article)
     }
+
+    let priceTotal = 0
+    for (let i = 0; i < getCartModels.length; i++) {
+
+      const { quantity, products } = getCartModels[i]
+      const {preco} = products
+      priceTotal += preco * quantity
+    }
+
+    let count = 0
+
+    for (let i = 0; i < getCartModels.length; i++) {
+
+      const { quantity, products } = getCartModels[i]
+      count += 1 * quantity
+    }
+
+    this._priceModalTotal.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(priceTotal)
+    this._modalCountTotal.innerText = count
 
     const buttonRemove = document.querySelectorAll('.button-remove')
     this.captureButtonsRemove(buttonRemove)
